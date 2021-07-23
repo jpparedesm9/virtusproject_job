8e37e81277e59fe0a477a4b560b5992d62b51119
jpparedesm9 <44152812+jpparedesm9@users.noreply.github.com>
2021-07-23 09:06:57 -0500

    Update cron-controllers.js

--- controllers/cron-controllers.js
+++ controllers/cron-controllers.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 const Variant = require('../models/variant');
 const axios = require('axios').default;
 
-sleep=(ms)=>{
+sleep = (ms) => {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
@@ -15,23 +15,23 @@ updateWixStock = async (sku, price, quantity) => {
     sku, price, quantity
   };
   const wixResult = await axios.put(`https://firoos.wixsite.com/delypruebas/_functions/updateBySku`, objToUpdate);
-  
-  console.log("se ejecuta la peticioin",wixResult);
+
+  console.log("se ejecuta la peticioin", wixResult);
 }
 
 
 getStockToUpdate = async (prod) => {
-  let stockNew=0;
-  let price=0;
+  let stockNew = 0;
+  let price = 0;
 
   const ariesQuery = await axios.get(`http://aries.delyclar.com//individual/clar3901/${prod.SKU}`);
+
   if(ariesQuery.data!==null){
   if(typeof ariesQuery.data["PRECIO_A"]!=="undefined")
   {
   price=ariesQuery.data["PRECIO_A"];
   let existenciaPadre = 0;
-  
-  stockNew = parseInt(ariesQuery.data["EXISTENCIA"]);
+   stockNew = parseInt(ariesQuery.data["EXISTENCIA"]);
   if (typeof ariesQuery.data["TRANS_NIVEL_PRODUCTO"] !== "undefined"&&
   typeof ariesQuery.data["TRANS_COD_PADRE"] !== "undefined"&&
   typeof ariesQuery.data["TRANS_CANTIDAD_HIJO"] !== "undefined"
@@ -48,71 +48,102 @@ getStockToUpdate = async (prod) => {
     }
     else{
       nivelProducto=1;
-    }
-    
-  }
+    }  }
   if (cantidadHijo !== 0) stockNew = existenciaPadre * cantidadHijo;
   }
 }
   }
 
+      stockNew = parseInt(ariesQuery.data["EXISTENCIA"]);
+      if (typeof ariesQuery.data["TRANS_NIVEL_PRODUCTO"] !== "undefined" &&
+        typeof ariesQuery.data["TRANS_COD_PADRE"] !== "undefined" &&
+        typeof ariesQuery.data["TRANS_CANTIDAD_HIJO"] !== "undefined"
+      ) {
+        let nivelProducto = parseInt(ariesQuery.data["TRANS_NIVEL_PRODUCTO"]);
+        let codPadre = ariesQuery.data["TRANS_COD_PADRE"];
+        let cantidadHijo = parseInt(ariesQuery.data["TRANS_CANTIDAD_HIJO"]);
+        while (nivelProducto !== 1) {
+          const superiorQuery = await axios.get(`http://aries.delyclar.com//individual/clar3901/${codPadre}`);
+          nivelProducto = parseInt(superiorQuery.data["TRANS_NIVEL_PRODUCTO"]);
+          codPadre = superiorQuery.data["TRANS_COD_PADRE"];
+          existenciaPadre = superiorQuery.data["EXISTENCIA"];
+        }
+        if (cantidadHijo !== 0) stockNew = existenciaPadre * cantidadHijo;
+      }
+     }
 
-return {stockNew,price};
+  return { stockNew, price };
 }
 
-async function updateLocalAndExternalProduct(sku,stock,price){
-    let updateProduct = await Product.findOneAndUpdate({ "SKU": sku },
+async function updateLocalAndExternalProduct(sku, stock, price) {
+  try {
+  let updateProduct = await Product.findOneAndUpdate({ "SKU": sku },
     { "Price": price, "QuantityInStock": stock }, {
     returnOriginal: false
-    });
-    await updateWixStock(sku, price, stock);
+  });
+  await updateWixStock(sku, price, stock);
+  console.log("estamos actualizando");
+}
+  catch(error){
+    console.error(error);
+  }
 }
 
 async function updateProducts(docs) {
   for (let x in docs) {
-   await sleep(1000);
     let doc = docs[x];
+
+
     try {
-      if (doc.TrackInventory) {
-        if (doc.ManageVariants) {
+      if (doc.TrackInventory === 'true') {
+        if (doc.ManageVariants === 'true') {
           let variantObj = await Variant.find({ ProductId: doc["_id"] }, []);
           let variants = variantObj.map(variant => variant.toObject({ getters: true }));
-          let parentStock=0;
+          let parentStock = 0;
+
+          try {
           for (let y in variants) {
-            await sleep(1000);
+
             let variant = variants[y];
-            let currentPrice = getStockToUpdate(variant);
-            parentStock+=currentPrice.stockNew;
-            if (currentPrice.stockNew !== doc.QuantityInStock) {
-              let inStock=currentPrice.stockNew>0
-              let updateVariant = await Variant.findOneAndUpdate({ "_id": variantObj["_id"] }, 
-              {'$set': { "Stock.stock": currentPrice.stockNew, "Stock.inStock":inStock} }, {
-                returnOriginal: false
-              });
+            await sleep(500);
+            let currentPrice = await getStockToUpdate(variant);
+            let localStock = JSON.parse(variant.Stock);
+            parentStock += currentPrice.stockNew;
+            if (currentPrice.stockNew !== localStock.quantity) {
+              //console.log("variante diferente");
+              //console.log(variant);
+              
+              let inStock = currentPrice.stockNew > 0;
+              localStock["quantity"]=currentPrice.stockNew;
+              localStock["inStock"]=inStock;
+              let updateVariant = await Variant.findOneAndUpdate({ "VariantID": variant["VariantID"] },
+              {"Stock":JSON.stringify(localStock)}
+              , {
+                new: true, upsert: false
+            });
+           
               //await updateWixStock(doc.sku, doc.discountedPrice, stockNew);
             }
-            
+
           }
-          await updateLocalAndExternalProduct(doc.sku,parentStock,0); ///revisar precio a actualizar
+        }catch(error){
+          console.error(error);
+        }
+          console.log("avanza");
+          await updateLocalAndExternalProduct(doc.sku, parentStock, 12); ///revisar precio a actualizar
         }
         else {
-          let currentPrice = getStockToUpdate(doc);
-         // if (doc.QuantityInStock !== currentPrice.stockNew) {
-            await updateLocalAndExternalProduct(doc.sku,currentPrice.stockNew,currentPrice.price);
-            /*
-            await updateLocalAndExternalProduct(doc.sku,currentPrice.stockNew,currentPrice.price);
-            let updateProduct = await Product.findOneAndUpdate({ "SKU": "0802986226025" },
-              { "Price": rcurrentPrice.price, "QuantityInStock": currentPrice.stockNew }, {
-              returnOriginal: false
-            });
-            await updateWixStock(doc.sku, doc.discountedPrice, stockNew);
-*/
+          try {
+          let currentPrice = await getStockToUpdate(doc);
+          if (doc.QuantityInStock !== currentPrice.stockNew) {
+           await updateLocalAndExternalProduct(doc.sku, currentPrice.stockNew, currentPrice.price);
 
-
-
-         // }
+          }
+        }catch(error){
+          console.error(error);
         }
-
+        }
+        await sleep(1000);
       }
     } catch (error) {
       //console.error(error);
@@ -130,4 +161,3 @@ const syncProducts = async () => {
   }
 };
 exports.syncProducts = syncProducts;
-
